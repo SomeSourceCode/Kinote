4318a1f9fe506ecd7e7c55e316c6dd2a2d186ef8
SomeSourceCode <somesourcecode@gmail.com>
2026-01-27 22:22:04 +0100

    feat: create Command

--- src/main/java/de/unistuttgart/einf/moviemanager/command/CommandNode.java
+++ src/main/java/de/unistuttgart/einf/moviemanager/command/CommandNode.java
@@ -95,7 +95,7 @@ public abstract class CommandNode implements Comparable<CommandNode> {
 	}
 
 	private static final List<Class<? extends CommandNode>> nodePriority = List.of(
-
+			Command.class,
 	);
 
 	@Override
