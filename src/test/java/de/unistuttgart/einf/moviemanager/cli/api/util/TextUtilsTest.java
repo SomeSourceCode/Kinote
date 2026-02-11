b7e44d796754fdc1eef8878d6bc7ab0a1cf03e43
SomeSourceCode <somesourcecode@gmail.com>
2026-02-11 15:28:49 +0100

    test: add TextUtilsTest

---
+++ src/test/java/de/unistuttgart/einf/moviemanager/cli/api/util/TextUtilsTest.java
@@ -0,0 +1,69 @@
+package de.unistuttgart.einf.moviemanager.cli.api.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TextUtilsTest {
+
+	@Nested
+	class WrapTextTest {
+
+		private void assertWrapping(String input, int width, List<String> expected) {
+			assertWrapping(input, width, expected, "Failed to wrap text");
+		}
+
+		private void assertWrapping(String input, int width, List<String> expected, String message) {
+			final List<String> actual = TextUtils.wrapText(input, width);
+
+			assertEquals(expected, actual, () -> String.format(
+					"%s: \"%s\" (width: %d)", message, input, width
+			));
+		}
+
+		@Test
+		@DisplayName("Should wrap text")
+		void testWrap() {
+			assertWrapping("Hello World", 5, List.of("Hello", "World"));
+			assertWrapping("Hello World", 10, List.of("Hello", "World"));
+			assertWrapping("Hello World", 11, List.of("Hello World"));
+		}
+
+		@Test
+		@DisplayName("Should return empty list for empty string or null")
+		void testWrapEmpty() {
+			assertWrapping(null, 10, Collections.emptyList(), "Should return empty list for null");
+			assertWrapping("", 10, Collections.emptyList(), "Should return empty list empty string");
+			assertWrapping("   ", 10, Collections.emptyList(), "Should return empty list string for only spaces");
+		}
+
+		@Test
+		@DisplayName("Should collapse multiple spaces")
+		void testWrapMultipleSpaces() {
+			assertWrapping("A   B", 10, List.of("A B"), "Should collapse multiple spaces into one");
+			assertWrapping("  Lead Trail  ", 10, List.of("Lead Trail"), "Should remove leading and trailing spaces");
+		}
+
+		@Test
+		@DisplayName("Should split giant words")
+		void testWrapGiantWords() {
+			assertWrapping("ABCDE", 3, List.of("ABC", "DE"), "Should split words longer than max width");
+			assertWrapping("ABCDE", 2, List.of("AB", "CD", "E"), "Should split words longer than max width over multiple lines");
+			assertWrapping("Giant WORD0123456789", 10, List.of("Giant WORD", "0123456789"), "Should start giant word on the same line");
+		}
+
+		@Test
+		@DisplayName("Should preserve explicit newlines")
+		void testWrapPreservesLineBreaks() {
+			assertWrapping("A\nB", 10, List.of("A", "B"), "Should preserve explicit line breaks");
+			assertWrapping("A\n\nB", 10, List.of("A", "", "B"), "Should preserve explicit new lines between paragraphs");
+		}
+
+	}
+
+}
