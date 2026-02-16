98025d4e979dbf6fd5892c4cc0f8258e61fe6553
SomeSourceCode <somesourcecode@gmail.com>
2026-02-16 20:56:38 +0100

    feat: add top level media argument

--- src/main/java/de/unistuttgart/einf/moviemanager/command/CommandNode.java
+++ src/main/java/de/unistuttgart/einf/moviemanager/command/CommandNode.java
@@ -105,6 +105,7 @@ public abstract class CommandNode implements Comparable<CommandNode> {
 			IntegerArgument.class,
 			LongArgument.class,
 			CommandArgument.class,
+			TopLevelMediaArgument.class,
 			StringArgument.class
 	);
 
