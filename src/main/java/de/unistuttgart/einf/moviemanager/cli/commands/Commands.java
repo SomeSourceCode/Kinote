c2aa85f12bfc7cb233821a648080d5f3aed459dd
SomeSourceCode <somesourcecode@gmail.com>
2026-02-17 12:31:49 +0100

    fix: delete command not registered

--- src/main/java/de/unistuttgart/einf/moviemanager/cli/commands/Commands.java
+++ src/main/java/de/unistuttgart/einf/moviemanager/cli/commands/Commands.java
@@ -28,6 +28,7 @@ public class Commands {
 		SettingsCommand.register(dispatcher, cli);
 
 		CreateCommand.register(dispatcher, cli);
+		DeleteCommand.register(dispatcher, cli);
 
 		return dispatcher;
 	}
